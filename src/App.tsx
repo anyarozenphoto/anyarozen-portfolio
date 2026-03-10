c7d19fcef11fcd3ccaa7d40f3892e60fbedab99b
anyarozenphoto <anyarozenphoto@gmail.com>
2026-03-10 16:11:09 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
 // src/App.tsx
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SiteNav from "./components/SiteNav";
-import SiteFooter from "./components/SiteFooter";
-import Index from "./pages/Index";
-import Portraits from "./pages/Portraits";
-import Maternity from "./pages/Maternity";
-import Families from "./pages/Families";
-import Lifestyle from "./pages/Lifestyle";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import NotFound from "./pages/NotFound";
-
 const App = () => (
-  <BrowserRouter>
-    <SiteNav />
-    <main>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/portraits" element={<Portraits />} />
-        <Route path="/maternity" element={<Maternity />} />
-        <Route path="/families" element={<Families />} />
-        <Route path="/lifestyle" element={<Lifestyle />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </main>
-    <SiteFooter />
-  </BrowserRouter>
+  <div
+    style={{
+      color: "white",
+      background: "black",
+      fontSize: "32px",
+      padding: "40px",
+    }}
+  >
+    TEST APP RENDERS
+  </div>
 );
 
 export default App;
